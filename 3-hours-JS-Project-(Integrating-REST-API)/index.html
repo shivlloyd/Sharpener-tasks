3ff55343631a1fcaa9f52f1db3c28fe86ad7afe2
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-09-08 19:27:14 +0530

    completed

---
+++ 3-hours-JS-Project-(Integrating-REST-API)/index.html
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</head>
+<body class="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+    <header class="text-center py-3">
+        <h1>Sellers Admin Page</h1>
+    </header>
+    <section class="d-flex align-items-center justify-content-center mt-4">
+        <form id="my-form" class="p-4 border rounded">
+            <h2 class="mb-3">Add Items</h2>
+
+            <div id="msg" class="mb-3"></div>
+
+            <div class="mb-3">
+                <label for="price" class="form-label">Selling Price:</label>
+                <input type="text" id="price" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="name" class="form-label">Product Name:</label>
+                <input type="text" id="name" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="category" class="form-label">Choose a category:</label>
+                <select id="category" name="category" class="form-select">
+                    <option value="" selected disabled>Choose the category</option>
+                    <option value="clothing">Clothing and Apparel</option>
+                    <option value="electronics">Electronics</option>
+                    <option value="food">Food and Groceries</option>
+                </select>
+            </div>
+            
+            <input id="btn" class="btn btn-primary" type="submit" value="Add Product">
+
+            <div id="clothing" class="category-container">
+                <h2>Clothing and Apparel</h2>
+            </div>
+            <div id="electronics" class="category-container">
+                <h2>Electronics</h2>
+            </div>
+            <div id="food" class="category-container">
+                <h2>Food and Groceries</h2>
+            </div>
+
+        </form>
+    </section>
+
+    <script src="main.js"></script>
+    <!-- adding axios funtionality -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.0/axios.min.js"></script>
+</body>
+</html>
---
+++ 3-hours-JS-Project-(Integrating-REST-API)/main.js
@@ -0,0 +1,97 @@
+const crudAPIkey = 'https://crudcrud.com/api/218c0636f62148ef9f3ad6caa33c8acf/itemData';
+
+//change of events when takes input
+
+const myForm = document.querySelector('#my-form');
+const msg = document.querySelector('#msg');
+const itemPrice = document.querySelector('#price');
+const itemName = document.querySelector('#name');
+const itemCategory = document.querySelector('#category');
+const btn = document.querySelector('#btn');
+
+
+function addItemList(itemDataList){
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
+    li.appendChild(document.createTextNode(`${itemDataList.price} - ${itemDataList.name} - ${itemDataList.category}`));
+
+    // adding delete button on list
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = "btn btn-sm btn-danger mx-1";
+    deleteBtn.appendChild(document.createTextNode('Delete Order'));
+    li.appendChild(deleteBtn);
+
+    //adding list in category
+    if(itemDataList.category === 'clothing'){
+        document.querySelector('#clothing').appendChild(li);
+    }
+    if(itemDataList.category === 'electronics'){
+        document.querySelector('#electronics').appendChild(li);
+    }
+    if(itemDataList.category === 'food'){
+        document.querySelector('#food').appendChild(li);
+    }
+
+    //delete item when clicked
+    deleteBtn.addEventListener('click', (e) => {
+        var itemCrudAPI = crudAPIkey + `/${itemDataList._id}`;
+
+        // delete item
+        axios
+            .delete(itemCrudAPI)
+            .then()
+            .catch((error) => {
+                console.log(error);
+            })
+        
+        li.remove();
+
+    })
+}
+
+// rendering listItems stored in crud server
+window.addEventListener("DOMContentLoaded", () => {
+    axios
+        .get(crudAPIkey)
+        .then((response) => {
+            for(var i=0; i<response.data.length; i++){
+                addItemList(response.data[i]);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+})
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (itemPrice.value === '' || itemName.value === '' || itemCategory.value === '') {
+        msg.innerHTML = '*Please enter all fields';
+
+    } else {
+
+        //storing items as an object on crud-crud using axios
+        let allItems = {
+            price: `₹${itemPrice.value}`,
+            name: itemName.value,
+            category: itemCategory.value
+        };
+
+        axios
+            .post(crudAPIkey, allItems)
+            .then((response) => {
+                //calling addItemList function
+                addItemList(response.data)
+
+                //clear fields
+                itemPrice.value = '';
+                itemName.value = '';
+                itemCategory.value = '';
+                msg.innerHTML = ""; 
+            })
+            .catch((err) => {
+                msg.innerHTML = "*something wrong with the API server";
+            })
+    }
+});
