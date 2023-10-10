f6036123f23b67a2162e8bbaeafbb0ae61e74ec8
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-10-10 22:36:04 +0530

    task 2 - Manipulating the DOM - React States

---
+++ Advance React/task2 - Manipulating the DOM - React States/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetail from "./ExpenseDetail";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+
+const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
+  const clickHandler = () => {
+    setTitle('updated');
+    console.log(title);
+  };
+
+  const amountHandler = () => {
+    setAmount(100);
+  };
+  
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <ExpenseDetail
+          location = {props.location}
+          title = {title}
+          amount={amount}
+      />
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={amountHandler}>Change amount</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
