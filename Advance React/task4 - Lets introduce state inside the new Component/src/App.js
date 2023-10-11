dc13f2b8911b1eadf8702178a14179e08bbc3f7a
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-10-11 15:46:39 +0530

    task4 - Lets introduce state inside the new Component

---
+++ Advance React/task4 - Lets introduce state inside the new Component/src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const App = () => {
+  const expenses = [
+    {
+      locationOfExpenditure: "DMart",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      locationOfExpenditure: "Amazon",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      locationOfExpenditure: "Policy Bazaar",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      locationOfExpenditure: "Flipkart",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
