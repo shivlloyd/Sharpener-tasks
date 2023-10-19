ba9651dd03fd7e7dfc4b0b6cdabf44b8db5c043d
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-10-19 09:15:49 +0530

    added task4 - Refs and Controlled Components

---
+++ Styling , Debugging and Lifecyle Methods in React/task4 - Refs and Controlled Components/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge, uCollege) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          name: uName,
+          age: uAge,
+          college: uCollege,
+          id: Math.random().toString(),
+        },
+      ];
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </React.Fragment>
+  );
+}
+
+export default App;
