7bc51beb1cf7bf4f5ec9626e8644562892fe046a
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-11-05 19:17:00 +0530

    added local storage functionality

--- 3-hours-React-JS-Project/src/App.js
+++ 3-hours-React-JS-Project/src/App.js
@@ -5,16 +5,38 @@ import Orders from "./Orders";
 function App() {
   const [orderList, setOrderList] = useState([]);
 
+  // saving orderList items in localStorage
+  useEffect(() => {
+    orderList.map((order) => {
+      return localStorage.setItem(order.orderId, JSON.stringify(order));
+    });
+  }, [orderList]);
+
+  // loading data from local storage to orderList
+  useEffect(() => {
+    const loadedOrderList = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const orderData = JSON.parse(localStorage.getItem(key));
+      if (orderData) {
+        loadedOrderList.push(orderData);
+      }
+    }
+    setOrderList(loadedOrderList);
+  }, []);
+
   function addOrderList(orderDetail) {
     setOrderList((prevValue) => {
       return [...prevValue, orderDetail];
     });
   }
 
-  function deleteOrder(orderId){
+  function deleteOrder(orderId) {
     setOrderList((prevValue) => {
       return prevValue.filter((order) => order.orderId !== orderId);
-    })
+    });
+    // removes order item from local storage
+    localStorage.removeItem(orderId);
   }
 
   return (
--- 3-hours-React-JS-Project/src/Orders.js
+++ 3-hours-React-JS-Project/src/Orders.js
@@ -7,16 +7,48 @@ const Orders = ({ orderList, deleteOrder }) => {
   return (
     <div>
       <h2>Orders</h2>
-      <ul>
-        {orderList.map((order) => {
+      <h3>Table 1</h3>
+      {orderList.map((order) => {
+        if (order.table === "table1") {
           return (
             <li key={order.orderId}>
               {order.price}-{order.table}-{order.dish}{" "}
-              <button onClick={() => clickHandler(order.orderId)}>Delete Order</button>
+              <button onClick={() => clickHandler(order.orderId)}>
+                Delete Order
+              </button>
             </li>
           );
-        })}
-      </ul>
+        }
+        return "";
+      })}
+      <h3>Table 2</h3>
+      {orderList.map((order) => {
+        if (order.table === "table2") {
+          return (
+            <li key={order.orderId}>
+              {order.price}-{order.table}-{order.dish}{" "}
+              <button onClick={() => clickHandler(order.orderId)}>
+                Delete Order
+              </button>
+            </li>
+          );
+        }
+        return "";
+      })}
+      <h3>Table 3</h3>
+      {orderList.map((order) => {
+        if (order.table === "table3") {
+          return (
+            <li key={order.orderId}>
+              {order.price}-{order.table}-{order.dish}{" "}
+              <button onClick={() => clickHandler(order.orderId)}>
+                Delete Order
+              </button>
+            </li>
+          );
+        }
+        return "";
+      })}
     </div>
   );
 };
