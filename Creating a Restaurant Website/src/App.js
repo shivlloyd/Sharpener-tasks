897ca96c1c401cf4cd01493611f19a8125d9c20b
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-11-06 16:00:43 +0530

    Added Restaurant Summary

--- Creating a Restaurant Website/src/App.js
+++ Creating a Restaurant Website/src/App.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from "react";
 
 import Header from "./components/layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
