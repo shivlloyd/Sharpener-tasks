be81d00210de3b1e13f2a1133a5fe334f0b390dc
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-11-07 15:52:25 +0530

    Making the Items visible

--- Creating a Restaurant Website/src/components/Meals/Meals.js
+++ Creating a Restaurant Website/src/components/Meals/Meals.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = () => {
   return (
     <Fragment>
       <MealsSummary />
+      <AvailableMeals />
     </Fragment>
   );
 };
