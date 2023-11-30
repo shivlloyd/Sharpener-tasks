243c059a6fb919527778b46f9d1a3186d0e77ced
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-11-30 13:39:01 +0530

    task 7 Loading the application

---
+++ Building your Ecommerce Website/task-7 Loading the application/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+
+  async function fetchMoviesHandler() {
+    setIsloading(true);
+
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+
+    setMovies(transformedMovies);
+    setIsloading(false);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found No Movies!</p>}
+        {isLoading && <p>Loading...</p>}
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
