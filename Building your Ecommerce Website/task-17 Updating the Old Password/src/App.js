c6b66266cc79c60be0221134f2212aca89cf3269
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-12-02 16:47:21 +0530

    task-17 Updating the Old Password

---
+++ Building your Ecommerce Website/task-17 Updating the Old Password/src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import UserProfile from './components/Profile/UserProfile';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/auth' element={<AuthPage />} />
+          <Route path='/profile' element={<UserProfile />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
