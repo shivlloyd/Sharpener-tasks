328e86f72fc81e7446666836581d8e387a899189
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-12-12 16:20:21 +0530

    task 3 Complete your profile

---
+++ Expense Tracker/task-3 Complete your profile/src/pages/HomePage.js
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../Store/AuthContext";
+import classes from "./HomePage.module.css";
+
+const HomePage = () => {
+  const authCtx = useContext(AuthContext);
+  console.log(authCtx.isLoggedIn);
+  const navigate = useNavigate();
+
+  const profileHandler = () => {
+    navigate("./profile");
+  };
+
+  return (
+    <>
+      {authCtx.isLoggedIn ? (
+        <section className={classes.starting}>
+          <p>Welcome to Expense Tracker!!!</p>
+          <div className="classes.profile">
+            <p>Your profile is incomplete.</p>
+            <button onClick={profileHandler}>Complete Now</button>
+          </div>
+        </section>
+      ) : (
+        <h1>Welcome to Expense Tracker!!!</h1>
+      )}
+    </>
+  );
+};
+
+export default HomePage;
