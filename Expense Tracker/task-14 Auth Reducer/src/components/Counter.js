239bd3bd99f90616a19210f3605416b775d8df48
Shivam Kumar <shivamkumar.2121997@gmail.com>
2023-12-29 08:00:42 +0530

    task 14 Auth Reducer

---
+++ Expense Tracker/task-14 Auth Reducer/src/components/Counter.js
@@ -0,0 +1,47 @@
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/index";
+import classes from "./Counter.module.css";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment(2));
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement(2));
+  };
+
+  const incrementBy5Handler = () => {
+    dispatch(counterActions.increment(5));
+  };
+
+  const decrementBy5Handler = () => {
+    dispatch(counterActions.decrement(5));
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>-- {counter} --</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <div>
+        <button onClick={incrementBy5Handler}>Increment By 5</button>
+        <button onClick={decrementBy5Handler}>Decrement By 5</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
